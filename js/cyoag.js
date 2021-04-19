5112cf27559e5e3123002424004119dc8fd45100
IrfaanNuMan <irfaan_numan@hotmail.com>
2021-04-19 10:47:53 -0600

    stories added

---
+++ js/cyoag.js
@@ -0,0 +1,12 @@
+ var bleep= new Audio();
+	bleep.src= "CYOAG-MOE/audio/click.mp3";
+	
+	
+
+function helloFunction() {
+    alert('Hello Napier');
+ }
+ 
+ 
+ var Click = new Audio();
+Click.src ="audio/Click.mp3";
