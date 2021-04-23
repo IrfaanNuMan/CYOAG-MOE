cf1216dfa40c793ed49932c951d0072f229a9aba
IrfaanNuMan <irfaan_numan@hotmail.com>
2021-04-23 07:11:59 -0600

    unfinished contact page added

--- js/cyoag.js
+++ js/cyoag.js
@@ -1,40 +1,15 @@
 /*Plays click sound when header items are selected"*/
- var Click = new Audio();
+var Click = new Audio();
 Click.src ="Click.mp3";
+
+
+var Squirrel = new Audio();
+Click.src ="Squirrel.mp3";
 
 /* Back button function*/
 function goBack() {
   window.history.back();
   
-/* let btnBack = document.querySelector('button');
- btnBack.addEventListener('click', () => {
-	 window.history.back();
- });*/
- 
- 
- /*Header text animation*/
- const text = document.querySelector(".animate");
- const strText = text.textContent;
- const splitText = strText.split("");
- text.textContent = "";
- 
- for (let i=0; i < splitText.length; i++) {
-	 text.innerHTML += "<span>" + splitText[i] + "</span>";
- }
- 
-let char = 0;
-let timer = setInterval(onTick, 50);
 
-function onTick(){
-	const span = text.querySelectorAll('span')[char];
-	span.classList.add('fade');
-	char++
-	if(char == splitText.length){
-		complete();
-		return;
-	}
-}
-funtion complete(){
-	clearInterval(timer);
-	timer = null;
-}
+ 
+ 
