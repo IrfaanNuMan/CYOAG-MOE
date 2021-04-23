118d08fee46e8e981c8fad642aa31d9f8a059909
IrfaanNuMan <irfaan_numan@hotmail.com>
2021-04-22 21:38:37 -0600

    heading animation and privacy policy

--- js/cyoag.js
+++ js/cyoag.js
@@ -1,7 +1,8 @@
+/*Plays click sound when header items are selected"*/
  var Click = new Audio();
 Click.src ="Click.mp3";
 
-
+/* Back button function*/
 function goBack() {
   window.history.back();
   
@@ -9,3 +10,31 @@ function goBack() {
  btnBack.addEventListener('click', () => {
 	 window.history.back();
  });*/
+ 
+ 
+ /*Header text animation*/
+ const text = document.querySelector(".animate");
+ const strText = text.textContent;
+ const splitText = strText.split("");
+ text.textContent = "";
+ 
+ for (let i=0; i < splitText.length; i++) {
+	 text.innerHTML += "<span>" + splitText[i] + "</span>";
+ }
+ 
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+	const span = text.querySelectorAll('span')[char];
+	span.classList.add('fade');
+	char++
+	if(char == splitText.length){
+		complete();
+		return;
+	}
+}
+funtion complete(){
+	clearInterval(timer);
+	timer = null;
+}
